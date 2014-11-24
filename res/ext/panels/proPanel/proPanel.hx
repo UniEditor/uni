4227dc6926e833989bdf5118d8951383a5deda4b
Guoboism <guoboism@hotmail.com>
2014-11-24 18:15:39 +0800

    minor work for pro panel

---
+++ res/ext/panels/proPanel/proPanel.hx
@@ -0,0 +1,26 @@
+
+this.id = "uni.pro";
+
+this.onInit = function(){
+	
+}
+
+this.onPanelOpen = function(){
+	
+}
+
+this.onPanelClose = function(){
+	
+}
+
+var onLog = function(e){
+	
+}
+
+//on panel init (before first time open)
+
+//on panel open (reg functions)
+
+//on panel close
+
+//on global log msg
