d56560379f734d39f352940199b58210de87f4e3
deadkidsong <deadkidsong@hotmail.com>
2014-12-12 09:13:40 +0800

    minor

---
+++ res/ext/panels/assetPanel/assetPanel.hx
@@ -0,0 +1,22 @@
+
+this.id = "uni.asset";
+
+this.onInit = function(){
+	
+}
+
+this.onPanelOpen = function(){
+	trace("asset onPanelOpen");
+}
+
+this.onPanelClose = function(){
+	trace("asset onPanelClose");
+}
+
+//on panel init (before first time open)
+
+//on panel open (reg functions)
+
+//on panel close
+
+//on global log msg
